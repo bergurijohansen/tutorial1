df4c48ab6eb1fad6d08b734f457336e596cff699
bergurijohansen <bergurij@gmail.com>
2019-01-25 19:44:49 +0000

    Updates

---
+++ precache-manifest.28af9c224d4684572447f990a71d0354.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tutorial1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "23dfd2197f7d9205033a",
+    "url": "/tutorial1/static/js/runtime~main.23dfd219.js"
+  },
+  {
+    "revision": "024bc0a175e9f8507fc2",
+    "url": "/tutorial1/static/js/main.024bc0a1.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/tutorial1/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "024bc0a175e9f8507fc2",
+    "url": "/tutorial1/static/css/main.40f99a90.chunk.css"
+  },
+  {
+    "revision": "8e9b21e8996ec66d9837cdf45ea894e0",
+    "url": "/tutorial1/index.html"
+  }
+];
