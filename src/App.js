fd47f2b83af7c96eeb2e290cb9deeca921b6782b
bergurijohansen <bergurij@gmail.com>
2019-01-25 19:48:01 +0000

    more

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Bergur was here
+            Bergur was here, again
           </p>
           <a
             className="App-link"
